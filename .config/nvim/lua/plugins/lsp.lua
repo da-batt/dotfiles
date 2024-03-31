14457bd285c4b3a06f4a8588751ba958cfabd427
da-batt <dev.overbid492@silomails.com>
2024-03-31 12:45:04 +0200

    Setup mason lspconfig automatic server setup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,21 @@
 return	{
     {
 	"williamboman/mason.nvim",
-	config = function()
-	    require("mason").setup()
-	end
+	config = true
     },
     {
 	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+	    "williamboman/mason.nvim",
+	    "neovim/nvim-lspconfig",
+	},
 	config = function()
-	    require("mason-lspconfig").setup({ ensure_installed = { "lua_ls" }})
-	end
-    },
-    {
-	"neovim/nvim-lspconfig",
-	config = function()
-	    local lspconfig = require('lspconfig')
-	    lspconfig.lua_ls.setup {}
+	    require('mason-lspconfig').setup({ ensure_installed = { "lua_ls" }})
+	    require('mason-lspconfig').setup_handlers {
+		function (server_name)
+		    require('lspconfig')[server_name].setup {}
+		end
+	    }
 	end
     }
 }
