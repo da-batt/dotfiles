3478d535af2897297da26d428b81774c4d6e84ea
da-batt <dev.overbid492@silomails.com>
2024-04-08 20:03:58 +0200

    Add automatic html tag completion

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,15 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+    config = function ()
+	local configs = require("nvim-treesitter.configs")
+
+	configs.setup({
+	    ensure_installed = { "lua", "html", "bash", "css", "javascript", "typescript", "tsx", "yaml" },
+	    highlight = { enable = true },
+	    indent = { enable = true },
+	    autotag = { enable = true }
+	})
+
+    end
 }
